6bb75c17166d539f073b8b7dcd2181be57608271
yeongjin Ha <eclips1024@naver.com>
2023-02-12 21:04:03 +0900

    CloneCoding JSDOC 사용법

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,9 +1,21 @@
+//@ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url 
+ * @returns boolean
+ */
 export function init(config){
     return true;
 }
-
+/**
+ * Exit the program
+ * @param {number} code 
+ * @returns number
+ */
 export function exit(code){
-    return codde +1;
+    return code +1;
 }
 
 //이건 자바스크립트코드로 타입스크립트가 알 수 없음. index.ts로 가서 패키지인것처럼 사용해 볼 것임
